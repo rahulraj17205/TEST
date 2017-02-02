e94c87647b24b7e6c71ea39be209e8479789ce29
rahulraj17205 <rahulraj202315@gmail.com>
2017-02-02 17:31:10 +0530

    new chanhes in test project

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -11,12 +11,15 @@ class welcome extends CI_Controller {
 	 *	- or -
 	 * 		http://example.com/index.php/welcome/index
 	 *	- or -
-	 * Since this controller is set as the default controller in
+	
 	 * config/routes.php, it's displayed at http://example.com/
 	 *
 	 * So any other public methods not prefixed with an underscore will
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 fsfsf
+	 fsfs
+	 sfsfsf
 	 */
 	public function index()
 	{
